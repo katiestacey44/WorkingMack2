0f91116c809e2f71487f10bbe3a11f546153163f
KatieStacey <katiestacey44@gmail.com>
2023-11-28 08:54:03 -0500

    test classe

--- app/src/main/java/com/example/myapplication/Test.java
+++ app/src/main/java/com/example/myapplication/Test.java
@@ -4,32 +4,6 @@ import android.graphics.Path;
 
 public class Test {
 
-    public static void main(String[] args) {
-        // Example test for C1floor
-        testC1floor();
-    }
 
-    private static void testC1floor() {
-        System.out.println("Testing C1floor class...");
-
-        // Example parameters for C1floor constructor
-        float scaleValue = 2.0f;
-        int currentRoom = 110;
-        int nextRoom = 140;
-
-        // Create C1floor object
-        C1floor c1floor = new C1floor(scaleValue, currentRoom, nextRoom);
-
-        // Get the generated Path
-        Path path = c1floor.getPath();
-        System.out.println("Generated Path: " + path.toString());
-
-        // Example test for getX() and getY()
-        int roomToCheck = 120;
-        System.out.println("Scaled X-coordinate for Room " + roomToCheck + ": " + c1floor.getX(roomToCheck));
-        System.out.println("Scaled Y-coordinate for Room " + roomToCheck + ": " + c1floor.getY(roomToCheck));
-
-        System.out.println("C1floor class test completed.");
-    }
 }
 
